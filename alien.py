fd89eb6086d54d907449c83929eb7f0043dd4e03
pstrassmann <st32490@gmail.com>
2020-01-20 10:35:12 -0800

    Created alien ship fleet and added horizontal movement

--- alien.py
+++ alien.py
@@ -6,6 +6,21 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
-
+        self.settings = ai_game.settings
         # Load the alien image and set its rect attribute
         self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact horizontal position
+        self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right"""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
+
+
--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 3
+        self.alien_speed = 1.0
 
         # Bullet settings
         self.bullet_speed = 50.0
@@ -15,3 +16,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 is flag for right; -1 flag for left
+        self.fleet_direction = 1
